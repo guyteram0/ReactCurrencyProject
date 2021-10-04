b60b8fefe5ec17428c437107eafb4aa2a47ef970
GuyTeram <guyteram0@gmail.com>
2021-10-04 16:27:45 +0300

    Updates

---
+++ precache-manifest.995e106ba960222a5d72df9aecdcf529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4daabf3e2c2d3bdbb444c9b53f5fd9",
+    "url": "/ReactCurrencyProject/index.html"
+  },
+  {
+    "revision": "ba415cfdb26e2a326b49",
+    "url": "/ReactCurrencyProject/static/css/main.0b9c4f85.chunk.css"
+  },
+  {
+    "revision": "cbb97f4053e90f51800d",
+    "url": "/ReactCurrencyProject/static/js/2.78c0658f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactCurrencyProject/static/js/2.78c0658f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba415cfdb26e2a326b49",
+    "url": "/ReactCurrencyProject/static/js/main.090bdc68.chunk.js"
+  },
+  {
+    "revision": "73e91a4c93b94557d925",
+    "url": "/ReactCurrencyProject/static/js/runtime-main.891cfaf8.js"
+  }
+]);
